b31d017aa97f7401a7ae3dc0e9375d159bc8d260
Siddhesh Kuvalekar <sid.kuvalekar@gmail.com>
2018-07-27 21:15:28 -0400

    zeroBins

--- Assignment4PSA/src/main/java/edu/neu/coe/info6205/BirthdayProblemCouponCollector/Main.java
+++ Assignment4PSA/src/main/java/edu/neu/coe/info6205/BirthdayProblemCouponCollector/Main.java
@@ -6,7 +6,7 @@
 package edu.neu.coe.info6205.BirthdayProblemCouponCollector;
 
 import java.util.Random;
-
+import static edu.neu.coe.info6205.BirthdayProblemCouponCollector.Helper.hash;
 /**
  *
  * @author Siddhesh
@@ -18,42 +18,73 @@ public class Main {
         int coupon_count=0;
         Double count =0.0;
         int nodeCount=0;
-
+        
+     /***** Number of hashes/throws before the first collision in encountered ********/
         for(coupon_count=10000;coupon_count<110000;coupon_count+=10000) {
         nodeCount=0;
         for(int k=0;k<10;k++)
         {
             HashTableSepChain<Double, Integer> hashTable = new HashTableSepChain<>(coupon_count);
+            count = (double) coupon_count;
+            Random r = new Random();
 
-        count = (double) coupon_count;
-        Random r = new Random();
+            int i=0;
+            double randomValue=0.0;
 
-
-        int i=0;
-        double randomValue=0.0;
-
-        while(HashTableSepChain.chainCount!=coupon_count)
-        {
+            while(HashTableSepChain.chainCount!=coupon_count)
+            {
                 randomValue = 0.0 + (count - 0.0) * r.nextDouble();
-
                 hashTable.put(randomValue, i++);
 
-        }	
+            }	
 
-        nodeCount+=HashTableSepChain.nodeCount;
-        HashTableSepChain.nodeCount = 0;
-        HashTableSepChain.chainCount = 0;
-
+            nodeCount+=HashTableSepChain.nodeCount;
+            HashTableSepChain.nodeCount = 0;
+            HashTableSepChain.chainCount = 0;
         }
 
         System.out.println(count);
 
         System.out.println("number of hashes/throws after when all bins/slots are filled: "+ nodeCount/10);
 
-        double logVal = count * Math.log(count);
+        double firstCollision = count * Math.log(count);
 
-        System.out.println("Expected hashes before all the slots are filled: "+ logVal);
+        System.out.println("Expected hashes before all the slots are filled: "+ firstCollision);
         System.out.println();
         }
+        
+        /***** Number of hashes/throws before all bins/slots are filled ********/
+        
+        for(int slots=10000;slots<110000;slots+=10000) {
+            count = (double) slots;
+            int[] arr = new int[slots+1];
+
+            Random rnd = new Random();
+            int hashCountSum=0;
+
+            for(int k=0;k<20;k++) {
+                double randomValue=0.0;
+                int index=0;
+                int hashCount=0;
+
+                while(arr[index]<2)
+                {
+                    randomValue = 0.0 + (count - 0.0) * rnd.nextDouble();
+                    index = hash(slots, randomValue);			
+                    hashCount++;
+                    arr[index]++;
+                }	
+
+                hashCountSum+=hashCount;
+                java.util.Arrays.fill(arr, 0);
+           }
+            System.out.println();
+            System.out.println("slot count: "+slots);
+            System.out.println("number of hashes when the fist collision: "+hashCountSum/20 );
+            
+            double zeroBins= Math.sqrt(3.14*slots/2);
+            System.out.println("expected number of hashes before the fist collision: "+ zeroBins);
+        }
+        
     }
 }
